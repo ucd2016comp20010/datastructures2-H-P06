17fb3ce69bf5d451bc9d0fd89002fc033ef6d220
Hailey Palacat <hailey.palacat@ucdconnect.iegit config --global user.name Hailey>
2026-03-23 15:35:32 +0000

    heap priority queue tests pass

--- src/project20280/priorityqueue/HeapPriorityQueue.java
+++ src/project20280/priorityqueue/HeapPriorityQueue.java
@@ -17,6 +17,8 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
 
     protected ArrayList<Entry<K, V>> heap = new ArrayList<>();
 
+    int size = 0;
+
     /**
      * Creates an empty priority queue based on the natural ordering of its keys.
      */
@@ -44,32 +46,33 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      */
     public HeapPriorityQueue(K[] keys, V[] values) {
         // TODO
+
     }
 
     // protected utilities
     protected int parent(int j) {
         // TODO
-        return 0;
+        return (j-1)/2;
     }
 
     protected int left(int j) {
         // TODO
-        return 0;
+        return 2*j+1;
     }
 
     protected int right(int j) {
         // TODO
-        return 0;
+        return 2*j+2;
     }
 
     protected boolean hasLeft(int j) {
         // TODO
-        return false;
+        return left(j) < heap.size();
     }
 
     protected boolean hasRight(int j) {
         // TODO
-        return false;
+        return right(j) < heap.size();
     }
 
     /**
@@ -77,6 +80,9 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      */
     protected void swap(int i, int j) {
         // TODO
+        Entry<K, V> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
     }
 
     /**
@@ -85,6 +91,22 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      */
     protected void upheap(int j) {
         // TODO
+        while (j > 0) {
+            int p = parent(j);
+
+            if(compare(heap.get(j), heap.get(p)) >= 0){
+                break;
+            }
+
+            //since this is a minheap, the parent is supposed to be smaller
+            //so if you have j bigger than p, j becomes the new parent and
+            //the old parent now becomes j
+            Entry<K, V> temp = heap.get(j);
+            heap.set(j, heap.get(p));
+            heap.set(p, temp);
+            j = p;
+        }
+
     }
 
     /**
@@ -92,6 +114,29 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      */
     protected void downheap(int j) {
         // TODO
+
+        //if a node was to have a child, it would start with left, so you cannot start with a right child without a left one
+        while(hasLeft(j)){
+            int leftIndex =  left(j);
+            int smaller = leftIndex; //just by default for now
+
+            if(hasRight(j)){
+                int rightIndex = right(j);
+
+                //find the biggest child
+                if(compare(heap.get(rightIndex), heap.get(leftIndex)) < 0){
+                    smaller = rightIndex;
+                }
+            }
+
+            if(compare(heap.get(smaller), heap.get(j) ) < 0){
+                swap(j, smaller);
+                j = smaller;
+            }
+            else{
+                break;
+            }
+        }
     }
 
     /**
@@ -110,7 +155,9 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      */
     @Override
     public int size() {
+        //im not changing this anymore
         return heap.size();
+        //return size;
     }
 
     /**
@@ -131,10 +178,17 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
      * @return the entry storing the new key-value pair
      * @throws IllegalArgumentException if the key is unacceptable for this queue
      */
+
+
     @Override
     public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
         // TODO
-        return null;
+        checkKey(key);
+        Entry<K, V> newEntry = new PQEntry<>(key, value);
+        heap.add(newEntry);
+        size++;
+        upheap(heap.size()-1);
+        return newEntry;
     }
 
     /**
@@ -145,7 +199,15 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
     @Override
     public Entry<K, V> removeMin() {
         // TODO
-        return null;
+        if(heap.isEmpty()){
+            return null;
+        }
+
+        Entry<K, V> min = heap.get(0);
+        heap.set(0, heap.get(heap.size()-1));
+        heap.remove(heap.size()-1);
+        downheap(0);
+        return min;
     }
 
     public String toString() {
